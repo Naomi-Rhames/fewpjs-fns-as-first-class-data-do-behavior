b7eafdf1d5558106f12ae545a08f186ebd304433
Naomi-Rhames <naomirhames2020@gmail.com>
2021-08-05 13:24:55 -0500

    commit tree 9c57cdf5c269b1939bcc620f9683062cb1cd7e97

--- index.js
+++ index.js
@@ -22,10 +22,15 @@ function greet(e){
   const hour = parseInt(hourString)
   if(hour < 12 ){
     return "Good Morning"
-  } else if (hour > 12){
+  } else if (hour >= 12 && hour <= 17){
     return "Good Afternoon"
-  } else if (hour > 5){
+  } else if (hour > 17){
     return "Good Evening"
   }
+  
+}
+function displayMessage(e){
+
+  return document.querySelector("#greeting").innerHTML = e
   
 }
